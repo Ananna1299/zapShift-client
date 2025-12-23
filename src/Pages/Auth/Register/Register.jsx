be74b0cdee01fe5a7313df0273b6fd1ee93c15d8
Ananna1299 <anannapaul1299@gmail.com>
2025-12-23 10:56:00 +0600

    env set

--- src/Pages/Auth/Register/Register.jsx
+++ src/Pages/Auth/Register/Register.jsx
@@ -40,7 +40,7 @@ const Register = () => {
             //store image and get the photoURL
             const formData=new FormData();
             formData.append("image",profileImg)
-            const image_API_URL=`https://api.imgbb.com/1/upload?key=${import.meta.env. VITE_image_host}`
+            const image_API_URL=`https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_image_host}`
 
             axios.post(image_API_URL,formData)
             .then(res=>{
